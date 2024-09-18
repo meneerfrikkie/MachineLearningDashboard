b40aacdc834965d2fb47b1f7653161de84084354
meneerfrikkie <100700962+meneerfrikkie@users.noreply.github.com>
2024-09-18 19:24:49 +0200

    add: Mobile PWA Attempt #1

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,34 @@
 import legacy from '@vitejs/plugin-legacy'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), VitePWA({
+    registerType: 'autoUpdate',
+    injectRegister: false,
+
+    manifest: {
+      name: 'Dashboard 🦾',
+      short_name: 'Machine Learning Dashboard 🦾',
+      description: 'Machine Learning Dashboard 🦾',
+      theme_color: '#ffffff"',
+    },
+
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+      cleanupOutdatedCaches: true,
+      clientsClaim: true,
+    },
+
+    devOptions: {
+      enabled: false,
+      navigateFallback: 'index.html',
+      suppressWarnings: true,
+      type: 'module',
+    },
+  })],
   test: {
     globals: true,
     environment: 'jsdom',
